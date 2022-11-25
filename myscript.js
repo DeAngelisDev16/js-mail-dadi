f23a234e81a6d924de2ba1951187e7fc8372b450
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-25 09:15:25 +0100

    Correzione gioco dadi

--- myscript.js
+++ myscript.js
@@ -13,33 +13,18 @@ proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 Buon lavoro e buon divertimento! */
 
 //gioco dadi:
-// recupero input e button dall'html con la creazione di due variabili:
-let playerNumber = document.getElementById('playerNumber');
-let button = document.getElementById('gameButton');
+
+let randomPlayerNumber = Math.floor(Math.random() * 6)+1;
+console.log(randomPlayerNumber);
+
+let randomComputerNumber = Math.floor(Math.random() * 6)+1;
+console.log(randomComputerNumber);
 
 
-
-
-// Al click del button, inizia il gioco.
-   // - viene generato un numero random dal computer;
-           //- se il numero random generato è maggiore di quello dell'utente, vincerà il computer;
-           //- in caso contrario vincerà l'utente;
-
-button.addEventListener('click', function(){
-    for (i = 1; i <=6; i++){
-        let randomNumber = Math.floor(Math.random() * 7);
-        console.log(randomNumber);
-        
-    }
-
-    if (parseInt(playerNumber.value) < randomNumber){
-        document.getElementById('output').innerHTML = 'Vince il computer';
-    } else {
-        document.getElementById('output').innerHTML = 'Hai vinto!';
-
-    }
-
-    
-})           
-
-
+if (randomPlayerNumber > randomComputerNumber){
+    console.log('Hai vinto!');
+} else if (randomPlayerNumber < randomComputerNumber){
+    console.log('Hai perso contro il computer!');
+} else {
+    console.log('Parità!');
+}
